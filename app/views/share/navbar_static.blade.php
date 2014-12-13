cbce1f65b2fef465199b43fcf84e444184f3821d
Tran Khanh <trankhanh.tk.kt@gmail.com>
2014-12-13 15:38:30 +0700

    Fix navigation for mobile

--- app/views/share/navbar_static.blade.php
+++ app/views/share/navbar_static.blade.php
@@ -1,6 +1,14 @@
 <nav class="navbar navbar-default navbar-fixed-top">
     <div class="container-fluid">
         @include('share.top_logo')
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            </button>
+        </div>
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
                 <li><a href="/">Home</a></li>
