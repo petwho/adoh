deddf968573cf3fcb2eb9701ebf3615153a71fdf
Tran Khanh <trankhanh.tk.kt@gmail.com>
2014-12-14 16:57:19 +0700

    Fix hardcode post image

--- app/views/post.blade.php
+++ app/views/post.blade.php
@@ -2,13 +2,11 @@
 <div class="container-fluid posts">
   @include('share.navbar_static')
 
-  <!-- @include('share.intro') -->
-
   <div class="row gallery-post">
-    <div class="col-xs-12 col-md-8 right-sidebar">
+    <div class="col-xs-12 col-md-8 right-sidebar" style="min-height: 700px;">
       <div class="row">
         <div class="col-xs-12 no-padding">
-          <img src="http://vinz.artlessthemes.com/horizontal-menu/wp-content/uploads/2014/10/about-section-headline.jpg" class="img-responsive">
+          <img src="{{$post->image}}" class="img-responsive">
         </div>
       </div>
       <div class="row content">
